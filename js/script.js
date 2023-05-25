fdd1b2e6de657cf8f79f893421e8f6d74835fd3c
Ashraf <makkiashraf08@gmail.com>
2023-05-24 20:05:57 -0400

    Optimizing our site

---
+++ js/script.js
@@ -0,0 +1,59 @@
+"use strict";
+
+// Make mobile navigation work
+
+const btn = document.querySelector(".btn-mobile-nav");
+const header = document.querySelector(".header");
+
+btn.addEventListener("click", function () {
+  header.classList.toggle("nav-open");
+});
+
+// Implement smooth scrolling for all devices
+const allLinks = document.querySelectorAll("a:link");
+
+allLinks.forEach(function (link) {
+  link.addEventListener("click", function (e) {
+    e.preventDefault();
+    const href = link.getAttribute("href");
+
+    if (href === "#") {
+      window.scrollTo({
+        top: 0,
+        behavior: "smooth",
+      });
+    }
+
+    if (href !== "#" && href.startsWith("#")) {
+      const el = document.querySelector(href);
+      el.scrollIntoView({ behavior: "smooth" });
+    }
+
+    if (link.classList.contains("main-nav-link")) {
+      header.classList.toggle("nav-open");
+    }
+  });
+});
+
+const element = document.getElementById("how");
+console.log(element);
+
+function checkFlexGap() {
+  var flex = document.createElement("div");
+  flex.style.display = "flex";
+  flex.style.flexDirection = "column";
+  flex.style.rowGap = "1px";
+
+  flex.appendChild(document.createElement("div"));
+  flex.appendChild(document.createElement("div"));
+
+  document.body.appendChild(flex);
+  var isSupported = flex.scrollHeight === 1;
+  flex.parentNode.removeChild(flex);
+  console.log(isSupported);
+
+  if (!isSupported) document.body.classList.add("no-flexbox-gap");
+}
+checkFlexGap();
+
+console.log("Hello World!");
